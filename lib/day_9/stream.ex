997141f537298365aef2ee26857ffbfe8c36036f
Alexander Borovykh <immaculate.pine@gmail.com>
2017-12-11 21:32:27 +0100

    Day 9, pt. 2 solved

--- lib/day_9/stream.ex
+++ lib/day_9/stream.ex
@@ -7,52 +7,52 @@ defmodule Day9.Stream do
 
   @doc """
       iex> Day9.Stream.read("{}")
-      1
+      {1, 0}
       iex> Day9.Stream.read("{{{}}}")
-      6
+      {6, 0}
       iex> Day9.Stream.read("{{},{}}")
-      5
+      {5, 0}
       iex> Day9.Stream.read("{{{},{},{{}}}}")
-      16
+      {16, 0}
       iex> Day9.Stream.read("{<a>,<a>,<a>,<a>}")
-      1
+      {1, 4}
       iex> Day9.Stream.read("{{<ab>},{<ab>},{<ab>},{<ab>}}")
-      9
+      {9, 8}
       iex> Day9.Stream.read("{{<!!>},{<!!>},{<!!>},{<!!>}}")
-      9
+      {9, 0}
       iex> Day9.Stream.read("{{<a!>},{<a!>},{<a!>},{<ab>}}")
-      3
+      {3, 17}
   """
   def read(data) do
     data
     |> String.split("", trim: true)
-    |> read(0, 0, false)
+    |> read(0, 0, 0, false)
   end
 
-  defp read([], score, _level, _garbaged) do
-    score
+  defp read([], score, _level, garbage_counter, _garbaged) do
+    {score, garbage_counter}
   end
 
-  defp read([head | tail], score, level, false) do
+  defp read([head | tail], score, level, garbage_counter, false) do
     case head do
-      "{" -> read(tail, score, level + 1, false)
-      "}" -> read(tail, score + level, level - 1, false)
-      "<" -> read(tail, score, level, true)
-      ">" -> read(tail, score, level, false)
-      "!" -> ignore(tail, score, level, false)
-      _ -> read(tail, score, level, false)
+      "{" -> read(tail, score, level + 1, garbage_counter, false)
+      "}" -> read(tail, score + level, level - 1, garbage_counter, false)
+      "<" -> read(tail, score, level, garbage_counter, true)
+      ">" -> read(tail, score, level, garbage_counter, false)
+      "!" -> ignore(tail, score, level, garbage_counter, false)
+      _ -> read(tail, score, level, garbage_counter, false)
     end
   end
 
-  defp read([head | tail], score, level, true) do
+  defp read([head | tail], score, level, garbage_counter, true) do
     case head do
-      ">" -> read(tail, score, level, false)
-      "!" -> ignore(tail, score, level, true)
-      _ -> read(tail, score, level, true)
+      ">" -> read(tail, score, level, garbage_counter, false)
+      "!" -> ignore(tail, score, level, garbage_counter, true)
+      _ -> read(tail, score, level, garbage_counter + 1, true)
     end
   end
 
-  defp ignore([_ | tail], score, level, garbaged) do
-    read(tail, score, level, garbaged)
+  defp ignore([_ | tail], score, level, garbage_counter, garbaged) do
+    read(tail, score, level, garbage_counter, garbaged)
   end
 end

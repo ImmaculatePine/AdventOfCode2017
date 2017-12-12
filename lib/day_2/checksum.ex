a262d47e683831fd32f2deb3f114068421660ba0
Alexander Borovykh <immaculate.pine@gmail.com>
2017-12-12 22:21:18 +0100

    Day 2, pt. 2 solved

--- lib/day_2/checksum.ex
+++ lib/day_2/checksum.ex
@@ -1,26 +1,44 @@
 defmodule Day2.Checksum do
+  def read(filepath) do
+    filepath
+      |> File.stream!
+      |> Stream.map(&String.trim_trailing/1)
+      |> Stream.map(fn line -> line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1) end)
+      |> Enum.to_list
+  end
+
   @doc """
-      iex> input = \"\"\"
-      iex> 5 1 9 5
-      iex> 7 5 3
-      iex> 2 4 6 8
-      iex> \"\"\"
-      iex> Day2.Checksum.calculate(input)
+      iex> Day2.Checksum.calculate([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]])
       18
   """
   def calculate(input) do
     input
-      |> String.split("\n", trim: true)
-      |> Enum.map(&row_checksum(&1))
+      |> Enum.map(&(Enum.max(&1) - Enum.min(&1)))
       |> Enum.sum
   end
 
-  defp row_checksum(row_input) do
-    row = row_input
-      |> String.split(~r/\s/, trim: true)
-      |> Enum.map(&String.to_integer(&1))
-    min = Enum.min(row)
-    max = Enum.max(row)
-    max - min
+  @doc """
+      iex> Day2.Checksum.calculate2([[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]])
+      9
+  """
+  def calculate2(input) do
+    input
+      |> Enum.map(&find_dividable_pair/1)
+      |> Enum.map(fn {x, y} -> x / y end)
+      |> Enum.sum
+      |> round
+  end
+
+  @doc """
+    iex> Day2.Checksum.find_dividable_pair([5, 9, 2, 8])
+    {8, 2}
+    iex> Day2.Checksum.find_dividable_pair([9, 4, 7, 3])
+    {9, 3}
+    iex> Day2.Checksum.find_dividable_pair([3, 8, 6, 5])
+    {6, 3}
+  """
+  def find_dividable_pair(list) do
+    [pair] = for x <- list, y <- list, x > y && rem(x, y) == 0, do: {x, y}
+    pair
   end
 end

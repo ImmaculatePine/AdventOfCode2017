f9f62d46c6f3b306e7d9deb7b217ff263a4cbd9c
Alexander Borovykh <immaculate.pine@gmail.com>
2017-12-13 01:02:37 +0100

    Day 10, pt. 2 solved

--- lib/day_10/knot.ex
+++ lib/day_10/knot.ex
@@ -1,26 +1,84 @@
 defmodule Day10.Knot do
+  import Bitwise
+  @standard_length_suffix [17, 31, 73, 47, 23]
+
   @doc """
-      iex> Day10.Knot.hash([0, 1, 2, 3, 4], [3])
-      [2, 1, 0, 3, 4]
-      iex> Day10.Knot.hash([0, 1, 2, 3, 4], [3, 4, 1, 5])
-      [3, 4, 2, 1, 0]
+      iex> Day10.Knot.hash("")
+      "a2582a3a0e66e6e86e3812dcb672a272"
+      iex> Day10.Knot.hash("AoC 2017")
+      "33efeb34ea91902bb2f59c9920caa6cd"
+      iex> Day10.Knot.hash("1,2,3")
+      "3efbe78a8d82f29979031a4aa0b16a9d"
+      iex> Day10.Knot.hash("1,2,4")
+      "63960835bcdc130f0b66d7ff4f6a5a8e"
   """
+  def hash(string) do
+    hash(Enum.to_list(0..255), String.to_charlist(string) ++ @standard_length_suffix)
+  end
+
   def hash(list, lengths) do
-    hash(list, lengths, 0, 0)
-  end
-
-  def hash(list, [], _current_position, _skip_size) do
     list
+      |> get_sparse_hash(lengths)
+      |> get_dense_hash
   end
 
-  def hash(list, [current_length | rest_lengths], current_position, skip_size) do
+  defp get_sparse_hash(list, lengths) do
+    {sparse_hash, _, _} = 1..64
+      |> Enum.to_list
+      |> Enum.reduce({list, 0, 0}, fn (_, {acc, current_position, skip_size}) ->
+        hash_round(acc, lengths, current_position, skip_size)
+      end)
+    sparse_hash
+  end
+
+  defp get_dense_hash(list) do
+    list
+      |> Enum.chunk_every(16)
+      |> Enum.map(&xor_list/1)
+      |> Enum.map(&(Integer.to_string(&1, 16)))
+      |> Enum.map(&(String.pad_leading(&1, 2, "0")))
+      |> Enum.map(&String.downcase/1)
+      |> Enum.join("")
+  end
+
+  @doc """
+      iex> Day10.Knot.xor_list([65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22])
+      64
+  """
+  def xor_list([head | tail]) do
+    xor_list(tail, head)
+  end
+
+  defp xor_list([], acc) do
+    acc
+  end
+
+  defp xor_list([head | tail], acc) do
+    xor_list(tail, bxor(acc, head))
+  end
+
+  @doc """
+      iex> Day10.Knot.hash_round([0, 1, 2, 3, 4], [3])
+      {[2, 1, 0, 3, 4], 3, 1}
+      iex> Day10.Knot.hash_round([0, 1, 2, 3, 4], [3, 4, 1, 5])
+      {[3, 4, 2, 1, 0], 4, 4}
+  """
+  def hash_round(list, lengths) do
+    hash_round(list, lengths, 0, 0)
+  end
+
+  def hash_round(list, [], current_position, skip_size) do
+    {list, current_position, skip_size}
+  end
+
+  def hash_round(list, [current_length | rest_lengths], current_position, skip_size) do
     reversed_sublist = list
       |> sublist(current_position, current_length)
       |> Enum.reverse
     next_position = recursive_position(current_position + current_length + skip_size, length(list))
     list
       |> replace(reversed_sublist, current_position)
-      |> hash(rest_lengths, next_position, skip_size + 1)
+      |> hash_round(rest_lengths, next_position, skip_size + 1)
   end
 
   def recursive_position(position, list_length) do

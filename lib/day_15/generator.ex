497a36ec8fa1c5620fd26cc86cf6f703b0199c64
Alexander Borovykh <immaculate.pine@gmail.com>
2017-12-16 00:50:42 +0100

    Day 15, pt. 2 solved

--- lib/day_15/generator.ex
+++ lib/day_15/generator.ex
@@ -4,16 +4,22 @@ defmodule Day15.Generator do
   @doc """
       iex> Day15.Generator.judge(65, 8921, 5)
       1
+      iex> Day15.Generator.judge({65, 4}, {8921, 8}, 5)
+      0
   """
+  def judge({start_a, mult_a}, {start_b, mult_b}, rounds) do
+    judge({start_a, 16807, mult_a}, {start_b, 48271, mult_b}, 0, rounds)
+  end
+
   def judge(start_a, start_b, rounds) do
     judge({start_a, 16807}, {start_b, 48271}, 0, rounds)
   end
 
-  def judge(_, _, score, 0) do
+  defp judge(_, _, score, 0) do
     score
   end
 
-  def judge(gen_a, gen_b, score, rounds_left) do
+  defp judge(gen_a, gen_b, score, rounds_left) do
     next_gen_a = next(gen_a)
     next_gen_b = next(gen_b)
     if matched?(next_gen_a, next_gen_b) do
@@ -33,7 +39,22 @@ defmodule Day15.Generator do
     {rem(value * factor, 2147483647), factor}
   end
 
-  def matched?({value_1, _}, {value_2, _}) do
-    (value_1 &&& 0xffff) == (value_2 &&& 0xffff)
+  @doc """
+      iex> Day15.Generator.next({65, 16807, 4})
+      {1352636452, 16807, 4}
+      iex> Day15.Generator.next({8921, 48271, 8})
+      {1233683848, 48271, 8}
+  """
+  def next({value, factor, multiplier}) do
+    new_value = rem(value * factor, 2147483647)
+    if rem(new_value, multiplier) == 0 do
+      {new_value, factor, multiplier}
+    else
+      next({new_value, factor, multiplier})
+    end
   end
+
+  def matched?({value_1, _}, {value_2, _}), do: matched?(value_1, value_2)
+  def matched?({value_1, _, _}, {value_2, _, _}), do: matched?(value_1, value_2)
+  def matched?(value_1, value_2), do: (value_1 &&& 0xffff) == (value_2 &&& 0xffff)
 end

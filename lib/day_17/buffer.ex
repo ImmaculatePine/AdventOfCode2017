6830b9cbb276e5f20127d3edce8cbb2666ea92d1
Alexander Borovykh <immaculate.pine@gmail.com>
2017-12-17 19:22:57 +0100

    Day 17, pt. 2 solved

--- lib/day_17/buffer.ex
+++ lib/day_17/buffer.ex
@@ -4,7 +4,13 @@ defmodule Day17.Buffer do
     Enum.at(buffer, current_position + 1)
   end
 
+  def value_after_0(total_buffer_size, step_size) do
+    emulate_build(total_buffer_size, step_size)
+  end
+
   @doc """
+  Builds the whole buffer and returns a tuple with {buffer, current_position}
+
       iex> Day17.Buffer.build(9, 3)
       {[0, 9, 5, 7, 2, 4, 3, 8, 6, 1], 1}
   """
@@ -24,6 +30,32 @@ defmodule Day17.Buffer do
       new_number + 1,
       new_position,
       List.insert_at(buffer, new_position, new_number)
+    )
+  end
+
+  @doc """
+  Emulates building the whole buffer of n elements and returns a value after the 0's element (with index == 1)
+
+      iex> Day17.Buffer.emulate_build(9, 3)
+      9
+  """
+  def emulate_build(n, step_size) do
+    emulate_build(n, step_size, 1, 0, 1, nil)
+  end
+
+  defp emulate_build(0, _step_size, _new_number, _current_position, _buffer_length, last_inserted_value) do
+    last_inserted_value
+  end
+
+  defp emulate_build(n, step_size, new_number, current_position, buffer_length, last_inserted_value) do
+    new_position = new_position(current_position, buffer_length, step_size)
+    emulate_build(
+      n - 1,
+      step_size,
+      new_number + 1,
+      new_position,
+      buffer_length + 1,
+      (if new_position == 1, do: new_number, else: last_inserted_value)
     )
   end
 
